814cc60626b1a1db872d720e76f891b1ed7b7287
LucasWerey <lucas.werey@edu.ece.fr>
2023-03-05 19:54:29 +0100

    Login page done

--- steam_project/lib/components/buttons/connection_button.dart
+++ steam_project/lib/components/buttons/connection_button.dart
@@ -17,25 +17,31 @@ class ConnectionButton extends StatelessWidget {
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 22),
             child: Container(
-                height: 46.89,
-                margin: const EdgeInsets.all(0.5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(3.52),
-                  color: filled
-                      ? const Color.fromARGB(255, 99, 106, 246)
-                      : Colors.transparent,
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 99, 106, 246),
-                    width: 2,
-                  ),
+              height: 46.89,
+              margin: const EdgeInsets.all(0.5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3.52),
+                color: filled
+                    ? const Color.fromARGB(255, 99, 106, 246)
+                    : Colors.transparent,
+                border: Border.all(
+                  color: const Color.fromARGB(255, 99, 106, 246),
+                  width: 2,
                 ),
-                child: OutlinedButton(
-                  onPressed: () => onPressed(),
+              ),
+              child: OutlinedButton(
+                onPressed: () => onPressed(),
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(width: 0, color: Colors.transparent),
+                ),
+                child: Center(
                   child: Text(buttonText,
                       style: const TextStyle(
                           color: Colors.white,
                           fontSize: 15.24,
                           fontFamily: 'Proxima')),
-                ))));
+                ),
+              ),
+            )));
   }
 }
